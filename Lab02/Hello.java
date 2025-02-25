0117f7ef067658211cabf1a58dab8d98c187d722
saywhy2me <123920271+saywhy2me@users.noreply.github.com>
2025-02-24 16:08:45 -0800

    Check 1,2

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("212336086 - This line was added by Mukesh Mehmi.");
 		System.out.println("220137542 - This line was added by Brandon Bradley.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by Saul Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");

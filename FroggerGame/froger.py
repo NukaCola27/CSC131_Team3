5202dc0d137493d41c7d82774a4a5d8cb359298a
saywhy2me <jordan707orion@gmail.com>
2025-03-19 17:03:23 -0700

    ADDED A BUNVH OF COLORS

--- FroggerGame/froger.py
+++ FroggerGame/froger.py
@@ -15,6 +15,26 @@ pygame.display.set_caption("Frogger Clone")
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
+BLUE = (0,0,255)
+TURQUOISE = (0,255,255)
+YELLOW = (255,255,0)
+ORANGE = (255,165,0)
+PINK = (255,192,203)
+PURPLE = (128,0,128)
+BROWN = (165,42,42)
+BLACK = (0,0,0)
+GRAY = (128,128,128)
+DARK_GRAY = (169,169,169)
+LIGHT_GRAY = (211,211,211)
+GOLD = (255,215,0)
+SILVER = (192,192,192)
+BRONZE = (205,127,50)
+MAROON = (128,0,0)
+NAVY = (0,0,128)
+OLIVE = (128,128,0)
+TEAL = (0,128,128)
+    
+
 
 # Player class
 class Player:
